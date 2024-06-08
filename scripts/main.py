5cdc0d5008956610237dce40f1fb53962c51844e
omri-minubo-bit <omri.niri@minubo.com>
2024-06-08 19:42:04 +0200

    added chart and saving as png

--- scripts/main.py
+++ scripts/main.py
@@ -6,7 +6,7 @@ from scripts.plotter import plot_portfolio
 def main():
     ctx = get_ctx()
     portfolio = get_portfolio(ctx)
-    # plot_portfolio(portfolio)
+    plot_portfolio(portfolio)
 
 
 if __name__ == '__main__':
--- scripts/plotter.py
+++ scripts/plotter.py
@@ -1,2 +1,45 @@
-def plot_portfolio():
-    ...
+import os
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+def get_title(df):
+    title = f'Portfolio- ' + ', '.join(df.columns)
+    return title
+
+
+def get_outputs_dir() -> str:
+
+    current_dir = os.getcwd()
+    parent_dir = os.path.dirname(current_dir)
+    outputs_dir = os.path.join(parent_dir, 'outputs')
+
+    return outputs_dir
+
+
+def plot_portfolio(df: pd.DataFrame):
+    """
+    Creates a chart, saves it as a png, and displays it.
+    :param df: the portfolio df, with index of dtype('<M8[ns]')
+    """
+    title = get_title(df)
+    outputs_dir = get_outputs_dir()
+    if not os.path.exists(outputs_dir):
+        os.makedirs(outputs_dir)
+    img_path = os.path.join(outputs_dir, title + '.png')
+
+    # Plot the portfolio
+    plt.figure(figsize=(10, 6))
+    for column in df.columns:
+        plt.plot(df.index, df[column], label=column)
+    plt.title(title)
+    plt.xlabel('Date')
+    plt.ylabel('Value')
+    plt.legend()
+    plt.grid(True)
+
+    # Save the plot as PNG
+    plt.savefig(img_path)
+
+    # Display the plot
+    plt.show()
